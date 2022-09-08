b52c6b8d36344edba245396bd56d1bac853a0902
geogrey12muiruri <greggambugua@gmail.com>
2022-09-08 22:09:53 +0300

    completed the assaignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const task = e.target.task_description.value;
+    addToDoList(task);
+    e.target.reset();
+    
+  })
 });
+function addToDoList(task) {
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
+  li.innerText = `${task}   `;
+  deleteButton.innerText = "X";
+  deleteButton.style.border = "1px solid black";
+  deleteButton.addEventListener("click", deleteTask);
+  li.appendChild(deleteButton);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function deleteTask(e) {
+  e.target.parentElement.remove();
+}
